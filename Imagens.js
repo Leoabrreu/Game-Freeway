04a6dba5dabac9ce8753e6e54c8f9d3c06b18a2a
Leonardo de Abreu Rodrigues <111782800+Leoabrreu@users.noreply.github.com>
2022-10-02 20:36:18 -0300

    Códgo em Javascript das imagens utilizadas.

---
+++ Imagens.js
@@ -0,0 +1,26 @@
+ //código imagens e sons do jogo
+
+let imagemDaEstrada;
+let imagemAtor;
+let imagemCarro;
+let imagemCarro2;
+let imagemCarro3;
+
+//sons
+let somDaTrila;
+let somDaColisao;
+let somDoPonto;
+
+
+function preload(){
+  imagemDaEstrada = loadImage("imagens/estrada.png");
+  imagemDoAtor = loadImage("imagens/ator-1.png");
+  imagemCarro1 = loadImage("imagens/carro-1.png");
+  imagemCarro2 = loadImage("imagens/carro-2.png");
+  imagemCarro3 = loadImage("imagens/carro-3.png");
+  imagemCarros = [imagemCarro1, imagemCarro2, imagemCarro3, imagemCarro1, imagemCarro2, imagemCarro3]
+  
+  somDaTrilha = loadSound("sons/trilha.mp3");
+  somDaColisao = loadSound("sons/colidiu.mp3");
+  somDoPonto = loadSound("sons/pontos.wav");
+}
